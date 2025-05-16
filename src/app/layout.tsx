5dac29e3859ed8212d146381f3b10d90a0ca1ad6
jjo3-nd <jjo3@nd.edu>
2025-05-16 13:52:17 -0400

    Google Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import React from "react";
 import ReactGA from 'react-ga4';
 const TRACKING_ID = "G-0ZJD8NMBE7"; // your Measurement ID
 
-ReactGA.initialize('TRACKING_ID');
+ReactGA.initialize(TRACKING_ID);
 
 
 export const metadata: Metadata = {
