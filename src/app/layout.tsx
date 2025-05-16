85ae45ed09af63b0852169aafafe1b0ffadd42ef
jjo3-nd <jjo3@nd.edu>
2025-05-16 17:02:39 -0400

    Google Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import type { Metadata } from "next";
+// import type { Metadata } from "next";
 import "./globals.css";
 import React, { useEffect } from "react";
 import ReactGA from 'react-ga4';
